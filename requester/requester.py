d9ba1a44eaf1a3cd0ddbac5f3aff6878d94ed7ca
rodrigondec <rodrigondec@gmail.com>
2019-07-25 23:06:28 -0300

    requester with logging

--- requester/requester.py
+++ requester/requester.py
@@ -1,18 +1,23 @@
 from os import environ
 from time import sleep
+import logging
 
 from requests import get
 
+logging.basicConfig(level=logging.DEBUG)
+
 # Server url
-URL = environ.get('HOST_URL', 'http://0.0.0.0:8000/')
+URL = f'http://{environ.get("HOST_URL", "localhost")}:8000/'
+logging.debug(f'Host url for request: {URL}')
 
 n = 1
-
 if __name__ == '__main__':
     while True:
         # sending get request and saving the response as response object
+        logging.debug(f'Making request number {n}...')
         r = get(url=URL, params={'name': f'requester {n}'})
-        print(f'Request number {n} made! response received: {r.text}')
+        logging.debug(f'Request number {n} returned {r}')
+        logging.debug(f'Response received: {r.text}')
         n += 1
-        print('sleeping for 3 seconds...')
+        logging.debug('sleeping for 3 seconds...')
         sleep(3)
